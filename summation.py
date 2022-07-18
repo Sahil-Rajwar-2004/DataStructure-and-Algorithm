16a0cddd96d893ea48e6b3c9bccd381ce370d6fd
Sahil Rajwar <justsahilrajwar2004@gmail.com>
2022-07-18 15:20:45 +0530

    Update summation.py

--- summation.py
+++ summation.py
@@ -1,16 +1,15 @@
-result = []
-
 class Solution:
-    def summation(self,nums:list,target:int):
+    result = []
+    def sum_2_no(self,nums:list,target:int):
         for i in range(0,len(nums)):
             for j in range(0,len(nums)):
-                if nums[i]+nums[j] == target:
+                if (nums[i]+nums[j] == target):
                     part = []
                     part.append(nums[i])
                     part.append(nums[j])
-                    result.append(part)
-                    part = []
-        return result
+                    if part[::-1] not in self.result:
+                        self.result.append(part)
+        return self.result
 
-add = Solution()
-print(add.summation([2,7,11,15],9))
+sol = Solution()
+print(sol.sum_2_no([-9,-8,-7,-6,-5,-4,-3,-2,-1,0,1,2,3,4,5,6,7,8,9,0],6))
