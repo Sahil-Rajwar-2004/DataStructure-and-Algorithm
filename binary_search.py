267d3fd29f5f3b5d1f4ac56237efa91c38da1dbb
Sahil Rajwar <justsahilrajwar2004@gmail.com>
2022-08-05 23:18:09 +0530

    Add files via upload

---
+++ binary_search.py
@@ -0,0 +1,17 @@
+def binary_search(args,target):
+    lower = args[0]
+    upper = args[len(args)-1]
+    if args[0] == target:
+        return 0
+    elif args[len(args)-1] == target:
+        return args[len(args)-1]
+    else:
+        while lower <= upper:
+            mid = int((upper+lower)/2)
+            if args[mid] == target:
+                return mid
+            elif args[mid] > target:
+                upper = mid-1
+            elif args[mid] < target:
+                lower = mid+1
+        return f"{target} is not found in {args}"
